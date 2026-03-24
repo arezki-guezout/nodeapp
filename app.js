fdf31d93c485e8766d40d6dad34fd750515ca7d9
arezki-guezout <84807388+arezki-guezout@users.noreply.github.com>
2026-03-24 17:07:02 +0100

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Devops from MyProject V3 guezout!!!');
+  res.send('Hello Devops from MyProject V3 guezoutddddd!!!');
 });
 
 const PORT = process.env.PORT || 3000;
